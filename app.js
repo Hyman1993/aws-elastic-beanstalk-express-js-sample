5291b908daa3a5fa05bd4347621f369aa9ca902d
Hyman1993 <1392841128@qq.com>
2021-05-27 17:35:13 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! AWS Simple!'));
+app.get('/', (req, res) => res.send('Hello World! AWS Simple!Review By Mannul'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
